0d6db82b2c3a8c7e25b2a8b3d80546f43708a06e
Reha Demircan <153535205+rdmrcn@users.noreply.github.com>
2025-06-07 15:38:11 +0300

    Update ItemType.cs

--- dgd208-RehaDemircan-Spring2025/Enums/ItemType.cs
+++ dgd208-RehaDemircan-Spring2025/Enums/ItemType.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 
 namespace DG208_Spring2025_RehaDemircan
@@ -7,16 +7,16 @@ namespace DG208_Spring2025_RehaDemircan
     {
         public string Name { get; }
         public ItemType Type { get; }
-        public int HungerEffect { get; }
+        public int HealthEffect { get; }
         public int FunEffect { get; }
         public int SleepEffect { get; }
-        public int UseDurationMs { get; } // delay in ms when using item
+        public int UseDurationMs { get; }
 
-        public Item(string name, ItemType type, int hungerEffect, int funEffect, int sleepEffect, int useDurationMs)
+        public Item(string name, ItemType type, int healthEffect, int funEffect, int sleepEffect, int useDurationMs)
         {
             Name = name;
             Type = type;
-            HungerEffect = hungerEffect;
+            HealthEffect = healthEffect;
             FunEffect = funEffect;
             SleepEffect = sleepEffect;
             UseDurationMs = useDurationMs;
@@ -26,7 +26,7 @@ namespace DG208_Spring2025_RehaDemircan
         {
             Console.WriteLine($"Using {Name} on {pet.Name}...");
             await Task.Delay(UseDurationMs);
-            pet.ChangeStats(HungerEffect, FunEffect, SleepEffect);
+            pet.ChangeStats(HealthEffect, FunEffect, SleepEffect);
             Console.WriteLine($"{pet.Name}'s stats updated!");
         }
     }
